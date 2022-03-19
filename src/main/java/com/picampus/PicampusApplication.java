1b3ff28e2298c22baf8f42ff716877883045b174
2287512349@qq.com <2287512349@qq.com>
2022-03-19 20:07:37 +0800

    写配置文件

---
+++ src/main/java/com/picampus/PicampusApplication.java
@@ -0,0 +1,13 @@
+package com.picampus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PicampusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PicampusApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/picampus/PicampusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.picampus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PicampusApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
